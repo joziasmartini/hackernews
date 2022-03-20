c0d8143a33fd170713b81488973353862d46c901
Jozias Martini <jozias.martini@gmail.com>
2022-03-20 15:49:03 -0300

    Transform the App class component into a function component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,116 @@
-import { Component } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Posts from "./components/Posts";
 
-class App extends Component {
-  state = {
-    posts: [],
-  };
+function App() {
+  const apiBase = "https://hacker-news.firebaseio.com/v0/";
+  const [posts, setPosts] = useState([]);
 
-  componentDidMount() {
-    const apiBase = "https://hacker-news.firebaseio.com/v0/";
-    this.fetchAPI(apiBase);
-  }
-
-  async fetchAPI(apiBase) {
-    await fetch(apiBase + "topstories.json")
-      .then((res) => res.json())
-      .then((res) => res.slice(0, 20))
-      .then((res) => {
-        res.forEach((id) => {
-          this.fetchItem(id, apiBase);
-        });
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-
-  async fetchItem(id, apiBase) {
+  const fetchItem = useCallback(async (id, apiBase) => {
     var item = apiBase + "item/" + id + ".json";
 
     await fetch(item)
       .then((res) => res.json())
       .then((res) => {
-        this.setState({ posts: [...this.state.posts, res] });
+        setPosts((posts) => [...posts, res]);
       })
       .catch((e) => {
         console.log(e);
       });
-  }
+  }, []);
 
-  render() {
-    const { posts } = this.state;
+  const fetchAPI = useCallback(
+    async (apiBase) => {
+      await fetch(apiBase + "topstories.json")
+        .then((res) => res.json())
+        .then((res) => res.slice(0, 20))
+        .then((res) => {
+          res.forEach((id) => {
+            fetchItem(id, apiBase);
+          });
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    [fetchItem]
+  );
 
-    return (
-      <div className="App">
-        <Header
-          title="HackerNews API"
-          subtitle="Listing the top posts from the Hacker News"
-        />
-        <Nav options={["Posts", "About"]} />
-        <Posts posts={posts} />
-      </div>
-    );
-  }
+  useEffect(() => {
+    fetchAPI(apiBase);
+    return () => {
+      setPosts([]);
+    };
+  }, [fetchAPI]);
+
+  return (
+    <div className="App">
+      {console.log(posts)}
+      <Header
+        title="HackerNews API"
+        subtitle="Listing the top posts from the Hacker News"
+      />
+      <Nav options={["Posts", "About"]} />
+      <Posts posts={posts} />
+    </div>
+  );
 }
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     posts: [],
+//   };
+
+//   componentDidMount() {
+//     const apiBase = "https://hacker-news.firebaseio.com/v0/";
+//     this.fetchAPI(apiBase);
+//   }
+
+//   async fetchAPI(apiBase) {
+//     await fetch(apiBase + "topstories.json")
+//       .then((res) => res.json())
+//       .then((res) => res.slice(0, 20))
+//       .then((res) => {
+//         res.forEach((id) => {
+//           this.fetchItem(id, apiBase);
+//         });
+//       })
+//       .catch((e) => {
+//         console.log(e);
+//       });
+//   }
+
+//   async fetchItem(id, apiBase) {
+//     var item = apiBase + "item/" + id + ".json";
+
+//     await fetch(item)
+//       .then((res) => res.json())
+//       .then((res) => {
+//         this.setState({ posts: [...this.state.posts, res] });
+//       })
+//       .catch((e) => {
+//         console.log(e);
+//       });
+//   }
+
+//   render() {
+//     const { posts } = this.state;
+
+//     return (
+//       <div className="App">
+//         <Header
+//           title="HackerNews API"
+//           subtitle="Listing the top posts from the Hacker News"
+//         />
+//         <Nav options={["Posts", "About"]} />
+//         <Posts posts={posts} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
